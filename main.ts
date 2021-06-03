1b49c5705a9097bfcfad1fe97b5badea1beece09
Ada <70569108+adax0107@users.noreply.github.com>
2021-06-03 08:48:08 +0200

    jeg har ændret det hele

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+let x = 0
+input.onGesture(Gesture.Shake, function () {
+    x = randint(1, 3)
+    if (x == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (x == 2) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
 })
